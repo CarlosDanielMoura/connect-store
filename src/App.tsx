2177967331f57c6f14ebea030ee6c8af6ade16d6
Carlos Daniel <carlos.daniel.moura99@gmail.com>
2023-12-15 11:33:35 -0300

    ✨ feat(Adicionado componente home):

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
+import Home from "./pages/home";
+
 const App: React.FC = () => {
-  return (
-    <>
-      <div>
-        <p className="text-blue-700">Hello World</p>
-      </div>
-    </>
-  );
+  return <Home />;
 };
 
 export default App;
